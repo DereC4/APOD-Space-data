da464cb7945ee7d2eee4be8a97b7b49df66221f5
Derek Chen <53978637+DereC4@users.noreply.github.com>
2022-04-06 09:57:30 -0500

    stuff

--- APOD.py
+++ APOD.py
@@ -1,5 +1,22 @@
 import requests
+import json
 
-def getAPOD(key):
-    temp = requests.get("https://api.nasa.gov/planetary/apod?api_key={key}")
-    return "WIP"
+APIKEY = ""
+photoJSON = ""
+
+def helloWorld():
+    print("Hello World - "+__file__)
+
+def getAPOD():
+    global photoJSON
+    print(APIKEY)
+    temp = requests.get("https://api.nasa.gov/planetary/apod?api_key="+APIKEY)
+    temp2 = json.dumps(temp.json())
+    photoJSON = json.loads(temp2)
+    # print(photoJSON)
+    return photoJSON
+
+def getURL():
+    global photoJSON
+    # print(photoJSON)
+    return photoJSON.get('url')
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+from re import T
+import APOD
+from tkinter import *
+from tkinter.ttk import *
+from PIL import Image,ImageTk
 
 apikey = "03nFjsaeGH0euZf10kux3Cx1AfvidxiBxh8kWqPZ"
+APOD.APIKEY = apikey
+canvas = Tk()
 
+def initCanvas():
+    canvas.geometry("720x480")
+    canvas.title("Derek's Cool Space App")
+    canvas.resizable(False, False)
+    APODButton = Button(canvas,text ="Click to see the picture of the day!",command = loadAPODImage)
+    APODButton.pack()
 
+def loadAPODImage():
+    # APOD.helloWorld()
+    APOD.getAPOD()
+    photoWindow = Toplevel(canvas)
+    photoWindow.title("Astronomy Picture of the Day")
+    photoWindow.geometry("1980x1080")
+    # print(APOD.getURL())
+    astrURL = APOD.getURL()
+    astrImage = ImageTk.PhotoImage(Image.open(astrURL))
+    photoWindow.create_image(10,10,anchor=NW,image=astrImage)
+initCanvas()
+mainloop()
