8b6e0bb2ea430f1242c8ff4b1e7aec281ab7ce95
Derek Chen <53978637+DereC4@users.noreply.github.com>
2022-04-07 10:23:57 -0500

    stuff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from re import T
 import APOD
 from tkinter import *
 from tkinter.ttk import *
-from PIL import Image,ImageTk
+from PIL import Image
 import urllib.request
 import os
 
@@ -11,14 +11,12 @@ APOD.APIKEY = apikey
 canvas = Tk()
 
 def initCanvas():
-    global canvas
+    global canvas   
     canvas.geometry("720x480")
     canvas.title("Derek's Cool Space App")
     canvas.resizable(False, False)
-    APODButton = Button(canvas,text ="Click to see the picture of the day!",command = loadAPODImage)
-    APODButton.pack()
-
-
+    APODButton = Button(canvas,text ="Click to see the\ndaily space image!",command = loadAPODImage)
+    APODButton.pack(side=LEFT, padx=15)
 
 def loadAPODImage():
     global canvas
@@ -27,16 +25,20 @@ def loadAPODImage():
     astrURL = APOD.getURL()
     urllib.request.urlretrieve(astrURL, "tempimage.jpg")
     convert = Image.open(r"tempimage.jpg")
+    convert = convert.resize((720, 480), Image.ANTIALIAS)
     convert.save(r"tempimage.png")
-    
+    os.remove("tempimage.jpg")
     photoWindow = Toplevel()
     photoWindow.title("Astronomy Picture of the Day")
-    canvas2 = Canvas(photoWindow, height=600, width=600)
+    canvas2 = Canvas(photoWindow, height=480, width=780)
+    photoWindow.resizable(False, False)
     canvas2.pack()
     print(APOD.getURL())
+    
     my_image = PhotoImage(file='tempimage.png')
-    canvas2.create_image(0, 0, anchor=NW, image=my_image)
 
+    canvas2.create_image(0, 0, anchor=NW, image=my_image)
+    canvas2.create_text(text="Image of Day")
     canvas2.mainloop()
 
 def on_closing():
@@ -45,6 +47,7 @@ def on_closing():
         canvas.destroy()
     else:
         canvas.destroy()    
+
 initCanvas()
 canvas.protocol("WM_DELETE_WINDOW", on_closing)
 mainloop()
