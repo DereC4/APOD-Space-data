4187150aeb70c7dfb5b3e1ac7224f3490c326dda
Derek Chen <53978637+DereC4@users.noreply.github.com>
2022-04-11 09:38:29 -0500

    yay image works and added random image option

--- APOD.py
+++ APOD.py
@@ -1,5 +1,9 @@
 import requests
 import json
+from PIL import Image
+import urllib.request
+import os
+import random
 
 APIKEY = ""
 photoJSON = ""
@@ -9,14 +13,55 @@ def helloWorld():
 
 def getAPOD():
     global photoJSON
-    print(APIKEY)
+    print("API KEY: " + APIKEY)
     temp = requests.get("https://api.nasa.gov/planetary/apod?api_key="+APIKEY)
     temp2 = json.dumps(temp.json())
     photoJSON = json.loads(temp2)
-    # print(photoJSON)
     return photoJSON
 
 def getURL():
     global photoJSON
-    # print(photoJSON)
     return photoJSON.get('url')
+
+# Date must be between Jun 16, 1995 and {CURRENT DATE}
+def getRandomURL():
+    global photoJSON
+    print("API KEY: " + APIKEY)
+    tempyear = random.randint(1996, 2020)
+    tempmonth = random.randint(1,12)
+    tempday = random.randint(1,28)
+    temp = requests.get("https://api.nasa.gov/planetary/apod?api_key="+APIKEY+"&date="+str(tempyear)+"-"+str(tempmonth)+"-"+str(tempday))  
+    temp2 = json.dumps(temp.json())
+    photoJSON = json.loads(temp2)
+    if(tempmonth == 1):
+        tempmonth = "January"
+    elif(tempmonth == 2):
+        tempmonth = "February"
+    elif(tempmonth == 3):
+        tempmonth = "March"
+    elif(tempmonth == 4):
+        tempmonth = "April"
+    elif(tempmonth == 5):
+        tempmonth = "May"
+    elif(tempmonth == 6):
+        tempmonth = "June"
+    elif(tempmonth == 7):
+        tempmonth = "July"
+    elif(tempmonth == 8):
+        tempmonth = "August"
+    elif(tempmonth == 9):
+        tempmonth = "September"
+    elif(tempmonth == 10):
+        tempmonth = "October"
+    elif(tempmonth == 11):
+        tempmonth = "November"
+    elif(tempmonth == 12):
+        tempmonth = "December"        
+    return photoJSON.get('url'), tempyear, tempmonth, tempday
+
+def loadImage(astrURL):
+    urllib.request.urlretrieve(astrURL, "tempimage.jpg")
+    convert = Image.open(r"tempimage.jpg")
+    convert = convert.resize((720, 480), Image.ANTIALIAS)
+    convert.save(r"tempimage.png")
+    os.remove("tempimage.jpg")
--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from tkinter.ttk import *
 from PIL import Image
 import urllib.request
 import os
+from datetime import date
 
 apikey = "03nFjsaeGH0euZf10kux3Cx1AfvidxiBxh8kWqPZ"
 APOD.APIKEY = apikey
 canvas = Tk()
+todayDate = date.today()
+
+def initApp():
+    APOD.getAPOD()
 
 def initCanvas():
     global canvas   
@@ -17,29 +22,43 @@ def initCanvas():
     canvas.resizable(False, False)
     APODButton = Button(canvas,text ="Click to see the\ndaily space image!",command = loadAPODImage)
     APODButton.pack(side=LEFT, padx=15)
+    APODButton = Button(canvas,text ="Click to see a\n random space image!",command = loadRandomAPODImage)
+    APODButton.pack(side=LEFT, padx=15)
 
 def loadAPODImage():
     global canvas
-    # APOD.helloWorld()
-    APOD.getAPOD()
     astrURL = APOD.getURL()
-    urllib.request.urlretrieve(astrURL, "tempimage.jpg")
-    convert = Image.open(r"tempimage.jpg")
-    convert = convert.resize((720, 480), Image.ANTIALIAS)
-    convert.save(r"tempimage.png")
-    os.remove("tempimage.jpg")
+    APOD.loadImage(astrURL)
+    print("URL: " + astrURL)
+
     photoWindow = Toplevel()
-    photoWindow.title("Astronomy Picture of the Day")
+    photoWindow.title(("Astronomy Picture of the Day for: ",todayDate))
+    photoWindow.resizable(False, False)
+
     canvas2 = Canvas(photoWindow, height=480, width=1080)
-    photoWindow.resizable(False, False)x
-    print(APOD.getURL())
+    my_image = PhotoImage(file='tempimage.png')
+    canvas2.create_image(0, 0, anchor=NW, image=my_image)
     
+    # canvas2.create_text(text="Image of Day")
+    # saveImageButton = Button(canvas2,text ="Save Image")
+    # saveImageButton.pack(side=RIGHT,padx=15)
+    canvas2.pack()
+    canvas2.mainloop()
+
+def loadRandomAPODImage():
+    global canvas
+    astrURL, year, month, day = APOD.getRandomURL()
+    APOD.loadImage(astrURL)
+    print("URL: " +astrURL)
+
+    photoWindow = Toplevel()
+    photoWindow.title("Astronomy Picture of the Day for"+str(year)+" "+month+" "+str(day))
+
+    canvas2 = Canvas(photoWindow, height=480, width=1080)
+    photoWindow.resizable(False, False)
     my_image = PhotoImage(file='tempimage.png')
 
-    # canvas2.create_image(0, 0, anchor=NW, image=my_image)
-    # canvas2.create_text(text="Image of Day")
-    saveImageButton = Button(canvas2,text ="Save Image")
-    saveImageButton.pack(side=RIGHT,padx=15)
+    canvas2.create_image(0, 0, anchor=NW, image=my_image)
     canvas2.pack()
     canvas2.mainloop()
 
@@ -51,5 +70,6 @@ def on_closing():
         canvas.destroy()    
 
 initCanvas()
+initApp()
 canvas.protocol("WM_DELETE_WINDOW", on_closing)
 mainloop()
